183c6a5e84cf2850bb628fbfb98774f8aa588376
Brendon Otto <brendon@brendonotto.com>
2016-11-29 22:02:46 -0600

    Removed data from productListCtrl

--- app/products/productListCtrl.js
+++ app/products/productListCtrl.js
@@ -8,31 +8,6 @@
 
     function ProductListCtrl(productResource) {
         var vm = this;
-        /* vm.products = [
-            {"productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-001",
-            "releaseDate": "March 19, 2009",
-            "description": "Leaf rake with 48-inch handle.",
-            "cost": 9.00,
-            "price": 19.95,
-            "category": "garden",
-            "tags": [ "leaf", "tool" ],
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Leaf-Rake.png"
-            },
-            {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2013",
-            "description": "Curved claw steel hammer",
-            "cost": 1.00,
-            "price": 8.99,
-            "category": "toolbox",
-            "tags": [ "tool" ],
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon-Hammer.png" 
-            }];
-            */
 
         productResource.query(function(data) {
             vm.products = data;
@@ -44,7 +19,5 @@
             vm.showImage = !vm.showImage;
         };
     };
-
-
 
 }());
