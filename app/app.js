ea0ec7da8e20b17d7a9b7185ea3026cbb09f1944
Otto <BOTTO@hcc.com>
2016-12-07 14:47:37 -0600

    Inclusing of routing and modules supporting routing

--- app/app.js
+++ app/app.js
@@ -2,6 +2,29 @@
     "use strict";
     var app = angular.module("productManagement",
                              ["common.services",
+                              "ui.router",
                               "productResourceMock"]);
+    app.config(["$stateProvider, $urlRouterProvider", 
+        function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                .state("home", {
+                    url: "/",
+                    templateUrl: "app/WelcomeView.html"
+                })
+                //Products
+                .state("productList", {
+                    url: "/products",
+                    templateUrl: "app/products/productListView.html",
+                    controller: "ProductListCtrl as vm"
+                })
+                .state("productEdit", {
+                    url: "/products/edit/:productId",
+                    templateUrl: "app/products/productEditView.html",
+                    controller: "ProductEditCtrl as vm"
+                })
+    }]
+    );
 
 }());
