d4d2b399fa393bb36db65b79a1d5e693b9ecaf1c
Brendon Otto <brendon@brendonotto.com>
2016-12-29 13:34:20 -0600

    Visualization data with charts module completed

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@
                               "ui.router",
                               "productResourceMock",
                               "ui.mask",
+                              "angularCharts",
                               "ui.bootstrap"]);
     app.config(["$stateProvider", "$urlRouterProvider", 
         function($stateProvider, $urlRouterProvider) {
@@ -55,6 +56,18 @@
                         product: function(productResource, $stateParams) {
                             var productId = $stateParams.productId;
                             return productResource.get({productId: productId}).$promise;
+                        }
+                    }
+                })
+                .state("priceAnalytics", {
+                    url: "/priceAnalytics",
+                    templateUrl: "app/prices/priceAnalyticsView.html",
+                    controller: "PriceAnalyticsCtrl",
+                    resolve: {
+                        productResource: "productResource",
+
+                        products: function (productResource) {
+                            return productResource.query().$promise;
                         }
                     }
                 })
